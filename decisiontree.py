c99ba3b09ce180be43d3d949a55468801d199f8f
Jud Taylor <jud.taylor@protonmail.com>
2020-06-24 19:56:57 -0400

    Wednesday.

---
+++ decisiontree.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+test_data_file = 'data_banknote_authentication.csv'
+
+class DecisionTree():
+    
+    def __init__(self):
+        self.value = 2
+    
+    def fit(self):
+        pass
+    
+    def predict(self):
+        pass
+    
+    def get_value(self):
+        return self.value
